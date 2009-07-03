df4cc61a56084b4cbe5c0f741e967c1d22087bbb
Oliver Kreylos <kreylos@cs.ucdavis.edu>
2009-07-03 12:06:31 -0700

    Release version 1.3 revision 3
    Using tarball (with SHA1):
    978f25c2ce5b9e6b4dd840ca5f3e5f53d4ca34a4 *NCK-1.3.tar.gz

--- StructuralUnit.cpp
+++ StructuralUnit.cpp
@@ -1,21 +1,6 @@
 /***********************************************************************
 StructuralUnit - Base class for structural units in molecular
 arrangements.
-Copyright (c) 2003-2011 Oliver Kreylos
-
-The Nanotech Construction Kit is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the
-License, or (at your option) any later version.
-
-The Nanotech Construction Kit is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with the Nanotech Construction Kit; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ***********************************************************************/
 
 #include <assert.h>
@@ -64,7 +49,7 @@ StructuralUnit::StructuralUnit(int sNumVertices,Misc::File& file)
 	file.read(position);
 	Scalar quaternion[4];
 	file.read<Scalar>(quaternion,4);
-	orientation=Rotation(quaternion);
+	orientation=Rotation::fromQuaternion(quaternion);
 	
 	/* Read velocities: */
 	file.read(linearVelocity);
